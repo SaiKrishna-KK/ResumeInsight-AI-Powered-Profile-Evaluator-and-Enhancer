acb728f592969a056cc371afcd218cc6707278ca
Sai Krishna Vishnumolakala <saikrishna.v1970@gmail.com>
2023-04-02 12:31:04 +0530

    Update Node Server

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
-
+const path = require('path');
 const app = express();
 const port = 3001;
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, '.')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 const openaiApiKey = 'open-ai-api-key-here';
 
